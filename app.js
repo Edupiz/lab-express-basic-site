d2b99000170f228deb47a35d807047e01fdcf33a
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-02-07 17:43:39 +0100

    done

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express ()
+app.use(express.static("public"))
+
+app.get("/home", (req, res) => {
+res.sendFile(__dirname + "/views/home.html")
+})
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/views/about.html")
+    })
+app.get("/works", (req, res) => {
+     res.sendFile(__dirname + "/views/works.html")
+        })
+app.get("/images", (req, res) => {
+            res.sendFile(__dirname + "/views/images.html")
+               })
+app.listen(3000, ()=>{
+    console.log("escuchando puerto 3000");
+        });
+
+
+
+
+
+
+
+
+
